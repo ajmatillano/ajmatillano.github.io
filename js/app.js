b6c009543f7658289a21240f44f6009d66c3da1b
AJ <aj@acasyscapital.com>
2019-06-12 20:47:41 +0800

    disabled close button animation

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var menuToggle = function menuToggle() {
+    var element = document.getElementById("icon-menu");
+    var sidemenu = document.getElementById("sidemenu");
+    var displayValue = document.getElementById("sidemenu").style.display;
+    if (displayValue == "none") {
+      sidemenu.style.display = "block";
+    //  element.classList.toggle("menuAnim");
+    }
+    else {
+    //  element.classList.toggle("menuAnim");
+    //  element.classList.toggle("menuAnimReverse");
+    }
+};
