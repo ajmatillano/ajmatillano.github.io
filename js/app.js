70a170e4bce43ab5a6350f69d409ed6d6cef24fa
AJ <aj@acasyscapital.com>
2019-06-16 20:01:34 +0800

    fixed menu icon issue

--- js/app.js
+++ js/app.js
@@ -1,30 +1,46 @@
 var iconmenu = document.getElementById("icon-menu");
 var sidemenu = document.getElementById("sidemenu");
 var displayValue = document.getElementById("sidemenu").style.display;
+var sideMenuDisplayed = 0;
+
+console.log(sideMenuDisplayed, "begin");
 
 var menuToggle = function menuToggle() {
-    if (displayValue == "none") {
-      sidemenu.style.display = "block";
-      sidemenu.style.transition = "all .5s";
-    //  iconmenu.classList.toggle("menuAnim");
-    }
+    sidemenu.style.opacity = "1";
 };
 
-iconmenu.addEventListener("click", menuAnimReverse);
+//iconmenu.addEventListener("click", menuAnimReverse);
 iconmenu.addEventListener("mouseover", menuAnimExpand);
 iconmenu.addEventListener("mouseout", menuAnimReverse);
-//sidemenu.addEventListener("webkitTransitionEnd", menuAnim);
+sidemenu.addEventListener("webkitTransitionEnd", menuAnim);
+sidemenu.addEventListener("TransitionEnd", menuAnim);
 
 function menuAnimReverse(){
+   if (sideMenuDisplayed == 0) {
       iconmenu.classList.add("menuAnimReverse");
       iconmenu.classList.remove("menuAnimExpand");
+      console.log(sideMenuDisplayed, "mouseout");
+  }
 }
 function menuAnimExpand(){
+    if (sideMenuDisplayed == 0) {
       iconmenu.classList.add("menuAnimExpand");
       iconmenu.classList.remove("menuAnimReverse");
+      console.log(sideMenuDisplayed, "mouseover");
+    }
 }
-// function menuAnim(){
-//       iconmenu.classList.toggle("menuAnim");
-//       iconmenu.classList.remove("menuAnimExpand");
-//       iconmenu.classList.toggle("menuAnimReverse");
-// }
+function menuAnim(){
+  if (sideMenuDisplayed == 0) {
+    iconmenu.classList.add("menuAnim");
+    iconmenu.classList.remove("menuAnimExpand");
+    iconmenu.classList.remove("menuAnimReverse");
+    sideMenuDisplayed = 1;
+    console.log(sideMenuDisplayed, "transition end");
+  } else {
+    iconmenu.classList.remove("menuAnim");
+    iconmenu.classList.remove("menuAnimExpand");
+    iconmenu.classList.add("menuAnimReverse");
+    sideMenuDisplayed = 0;
+    console.log(sideMenuDisplayed, "transition end");
+  }
+}
